dd8beb6c9c4c957f87a83d89fc325a4fe74e3398
Timilehin Amolegbe <timilehinamolegbe1@gmail.com>
2024-07-25 15:49:30 +0100

    Passing argument works values

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/*
+////////////////////////////////
+//Default Parameter
+
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +29,28 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 //tO SKIP THE SECOND PARAMETER(numpassenger)
 createBooking('LH123', undefined, 1500);
+
+*/
+
+/////////////////////////
+//Passing arguements
+const flight = 'LH123';
+const timmy = {
+  name: 'Timmy Amolegbe',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check In');
+  } else {
+    alert('Wrong Passport!');
+  }
+};
+
+checkIn(flight, timmy);
+console.log(flight);
+console.log();
